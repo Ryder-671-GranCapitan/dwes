299685ff6e1a4a80210d51e2b27f6dd389f4ebd2
Ryder-671-GranCapitan <168223949+Ryder-671-GranCapitan@users.noreply.github.com>
2025-05-27 17:08:11 +0200

    reorganizacion

--- examen.com/EjerciciosFormularios/Ej04/includes/funciones2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  require("funciones.php");
-
-  function f2() {
-    echo "Function f2";
-  }
-
-?>
--- examen.com/exra507/vista/V_Error.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace exra507\vista;
-    use Exception;
-
-    class V_Error extends Exception {
-        
-    }
-?>
